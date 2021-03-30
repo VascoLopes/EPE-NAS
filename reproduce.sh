3f7fb2ce62153698e1bfc3b1e667ce1502cb1f64
VascoLopes <vascoferrinholopes@gmail.com>
2021-03-30 14:08:53 +0100

    Add files via upload

--- reproduce.sh
+++ reproduce.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-python search_metric.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 10 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
 
-python search_metric.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 100 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
 
-python search_metric.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 500 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
 
-python search_metric.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
-python search_metric.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --data_loc './datasets/cifar10'            --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar10 --trainval --data_loc './datasets/cifar10' --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset cifar100 --data_loc './datasets/cifar100'          --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
+python search.py --dataset ImageNet16-120 --data_loc './datasets/ImageNet16'  --n_runs $1 --n_samples 1000 --api_loc './datasets/NAS-Bench-201-v1_0-e61699.pth'
 
-echo "bath size 256"
 python process_results.py --n_runs $1
